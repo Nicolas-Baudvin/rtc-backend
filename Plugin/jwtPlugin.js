e2c6e2f093d4d01a5339198083ddfef18c258ba3
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-07-28 15:30:56 +0200

    jwt plugin mw

---
+++ Plugin/jwtPlugin.js
@@ -0,0 +1,31 @@
+const fp = require("fastify-plugin");
+const jwt = require('fastify-jwt');
+
+function jwtPlugin (fastify, opts, next) {
+    fastify.register(jwt, {
+        secret: process.env.TOKEN_KEY
+    });
+
+    fastify.decorate("authenticate", async function (request, reply) {
+        const { _id } = request.body;
+        const token = request.headers.authorization.split(" ")[1];
+        if (!token || !_id)
+        {
+            reply.code(403).send({ error: "Accès interdit." });
+        }
+        try
+        {
+            const decoded = await fastify.jwt.verify(token);
+            if (decoded._id !== _id)
+            {
+                reply.code(403).send("Accès interdit.");
+            }
+        } catch (err)
+        {
+            reply.send(err);
+        }
+    });
+    next();
+}
+
+module.exports = fp(jwtPlugin, { fastify: '>=1.0.0'});
