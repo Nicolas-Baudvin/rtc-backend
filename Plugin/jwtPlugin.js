dc10a2d0d183ea191a0a589c98b3f14b73367361
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-07-28 19:44:49 +0200

    add conditions on token properties

--- Plugin/jwtPlugin.js
+++ Plugin/jwtPlugin.js
@@ -1,31 +1,27 @@
-const fp = require("fastify-plugin");
+const fp = require('fastify-plugin');
 const jwt = require('fastify-jwt');
 
-function jwtPlugin (fastify, opts, next) {
+function jwtPlugin(fastify, opts, next) {
     fastify.register(jwt, {
-        secret: process.env.TOKEN_KEY
+        secret: process.env.TOKEN_KEY,
     });
 
-    fastify.decorate("authenticate", async function (request, reply) {
+    fastify.decorate('authenticate', async function (request, reply) {
         const { _id } = request.body;
-        const token = request.headers.authorization.split(" ")[1];
-        if (!token || !_id)
-        {
-            reply.code(403).send({ error: "Accès interdit." });
+        const token = request.headers?.authorization?.split(' ')[1];
+        if (!token || !_id) {
+            reply.code(403).send({ error: 'Accès interdit.' });
         }
-        try
-        {
+        try {
             const decoded = await fastify.jwt.verify(token);
-            if (decoded._id !== _id)
-            {
-                reply.code(403).send("Accès interdit.");
+            if (decoded._id !== _id) {
+                reply.code(403).send('Accès interdit.');
             }
-        } catch (err)
-        {
+        } catch (err) {
             reply.send(err);
         }
     });
     next();
 }
 
-module.exports = fp(jwtPlugin, { fastify: '>=1.0.0'});
+module.exports = fp(jwtPlugin, { fastify: '>=1.0.0' });
