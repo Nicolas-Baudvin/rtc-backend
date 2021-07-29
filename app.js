40cbc36e491631777fdd225264fc422ff611f03d
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-07-29 16:46:48 +0200

    format

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
 const fastify = require('fastify');
-const cors = require("fastify-cors");
-const socketIo = require("fastify-socket.io");
+const cors = require('fastify-cors');
+const socketIo = require('fastify-socket.io');
 const jwtPlugin = require('./Plugin/jwtPlugin');
 const userRoutes = require('./Router/UserRoutes');
-const mongoose = require('mongoose');
-require("dotenv").config();
-
+const socketEventController = require('./WebSocket');
+require('dotenv').config();
 
 function build(opts = {}) {
     const app = fastify({ logger: true, ...opts });
 
     app.register(cors, {
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     });
 
     app.register(socketIo, {
         cors: {
-            origin: "*",
-            methods: ["GET", "POST"],
-            "credentials": true
-        }
+            origin: '*',
+            methods: ['GET', 'POST'],
+            credentials: true,
+        },
     }).then(() => {
-        app.io.on("connection", (socket) => {
-            console.info("socket connected");
-            socket.on("login", (data) => {
-                console.log("login", data);
-            });
-        });
+        app.io.on('connection', (socket) => socketEventController(socket));
     });
 
-    app.register(jwtPlugin).after(err => { if (err) throw err });
+    app.register(jwtPlugin).after((err) => {
+        if (err) throw err;
+    });
     app.register(userRoutes);
 
     return app;
