9a9f9c683571f8f7d445a083e49cfda77b7f8e24
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-07-30 16:00:21 +0200

    delete room

---
+++ WebSocket/deleteRoom.js
@@ -0,0 +1,36 @@
+const Room = require('../Model/Room');
+const Message = require('../Model/Message');
+const isTokenValid = require('./Util/isTokenValid');
+
+async function deleteRoom(socket, data) {
+    if (!isTokenValid(data)) {
+        return socket.emit('failed authentication', {
+            error: 'Vous ne pouvez accéder à cette fonctionnalité, reconnectez vous.',
+        });
+    }
+
+    const { _id, name } = data.room;
+
+    if (!_id || !name) {
+        return socket.emit('delete error', {
+            error: 'Impossible de trouver ce chat !',
+        });
+    }
+
+    try {
+        await Room.deleteOne({ _id, name });
+        await Message.deleteMany({ roomId: _id });
+        return socket.emit('room deleted', {
+            success: true,
+            message: 'Le chat et tous les messages ont bien été supprimé',
+        });
+    } catch (e) {
+        console.error('server error : ', e);
+        return socket.emit(
+            'server fail',
+            'Le serveur a rencontré un problème.'
+        );
+    }
+}
+
+module.exports = deleteRoom;
