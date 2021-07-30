b6621b370b80caa644a3104b34c4d6a9803fb4e9
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-07-30 15:04:55 +0200

    add socket listener for creating a room

--- WebSocket/index.js
+++ WebSocket/index.js
@@ -1,8 +1,10 @@
 const socketAuthentication = require('./auth');
+const createRoom = require('./createRoom');
 
 function socketEventController(socket) {
     console.log("Quelqu'un s'est connecté");
     socket.on('login', (data) => socketAuthentication(socket, data));
+    socket.on('create room', (data) => createRoom(socket, data));
 }
 
 module.exports = socketEventController;
