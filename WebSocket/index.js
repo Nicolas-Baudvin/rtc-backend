8a60fc2c47021a0fcc6c0393f89240cb975dd823
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-07-30 16:00:45 +0200

    add socket listner for deleting a room

--- WebSocket/index.js
+++ WebSocket/index.js
@@ -1,10 +1,12 @@
 const socketAuthentication = require('./auth');
 const createRoom = require('./createRoom');
+const deleteRoom = require('./deleteRoom');
 
 function socketEventController(socket) {
     console.log("Quelqu'un s'est connecté");
     socket.on('login', (data) => socketAuthentication(socket, data));
     socket.on('create room', (data) => createRoom(socket, data));
+    socket.on('delete room', (data) => deleteRoom(socket, data));
 }
 
 module.exports = socketEventController;
