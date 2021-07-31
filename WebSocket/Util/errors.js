bfb2ec9a433c5ed5ebfaedb3170e777d9210328c
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-07-31 15:34:41 +0200

    add an error

--- WebSocket/Util/errors.js
+++ WebSocket/Util/errors.js
@@ -2,6 +2,7 @@ const errors = (data) => ({
     auth: 'Vous ne pouvez accéder à cette fonctionnalité, reconnectez vous.',
     noname: "Le champs 'nom du chat' est obligatoire",
     notfound: `Aucun chat avec le nom ${data.room.name} n'existe`,
+    leaveName: 'Le chat est introuvable',
 });
 
 module.exports = errors;
