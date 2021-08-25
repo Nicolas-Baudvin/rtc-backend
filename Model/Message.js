dc6e169b33d6f9ac6bd0c708f2d6624e658975ae
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-25 12:05:53 +0200

    update Message model

--- Model/Message.js
+++ Model/Message.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-const Room = require('./Room');
 
 const MessageSchema = new mongoose.Schema(
     {
         desc: { type: String, required: true },
         author: { type: String, required: true },
-        authorPicture: { type: String, required: true },
+        authorPicture: { type: String, required: false },
         roomId: { type: String, required: true },
     },
     { timestamps: true }
